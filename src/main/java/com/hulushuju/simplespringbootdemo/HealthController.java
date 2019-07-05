63ce1b62a2ae7d376f50388182cfc607b75096c6
layker <layke1993@163.com>
2019-07-05 13:34:51 +0800

    yaml image替换

--- src/main/java/com/hulushuju/simplespringbootdemo/HealthController.java
+++ src/main/java/com/hulushuju/simplespringbootdemo/HealthController.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by layker on 2018-12-10
  */
 @RestController
-@RequestMapping(value = "healthz")
+@RequestMapping(value = "actuator")
 public class HealthController {
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "healthz", method = RequestMethod.GET)
     public String healthz() {
+        System.out.println("==================健康检查healthz==================");
         return "ok";
+    }
+
+    @RequestMapping(value = "ready", method = RequestMethod.GET)
+    public String ready() {
+        System.out.println("==================就绪检查ready==================");
+        return "ready";
     }
 }
